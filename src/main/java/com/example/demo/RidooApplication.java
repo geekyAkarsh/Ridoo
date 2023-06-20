ef29e52474f20ca112b95ed4ff83b5f76f6dae4b
Akarsh <akarshsingh81@gmail.com>
2023-06-20 23:47:35 +0530

    first commit

---
+++ src/main/java/com/example/demo/RidooApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RidooApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(RidooApplication.class, args);
+	}
+
+}
